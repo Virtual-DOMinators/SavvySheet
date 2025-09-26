ec006c45fb67c72b30308175a88a673756de719a
Tom Larsson <tmlsn@hotmail.com>
2025-09-26 14:04:30 +0200

    La in så att filnamnet skickas med

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ type ExcelRow = { [key: string]: string | number | boolean | null };
 function App() {
   const [data, setData] = useState<ExcelRow[]>([]);
   const [columns, setColumns] = useState<ExportColumn<ExcelRow>[]>([]);
+  const [filename, setFilename] = useState<string | undefined>(undefined);
 
   const handleDataChange = (jsonData: ExcelRow[]) => {
     setData(jsonData);
-
     if (jsonData.length > 0) {
       const keys = Object.keys(jsonData[0]);
       setColumns(keys.map((key) => ({ field: key, headerName: key })));
@@ -21,12 +21,19 @@ function App() {
   return (
     <div>
       <Header />
-      <UploadFile onDataParsed={handleDataChange} />
+      <UploadFile
+        onDataParsed={(parsedData, fileName) => {
+          setData(parsedData);
+          handleDataChange(parsedData);
+          setFilename(fileName);
+        }}
+      />
+
       <EditableTable
         data={data as { [key: string]: unknown }[]}
         dataOnChange={setData as (newData: { [key: string]: unknown }[]) => void}
       />
-      <ExportButton data={data} columns={columns} filename="export.pdf" />
+      <ExportButton data={data} columns={columns} originalFileName={filename} />
     </div>
   );
 }
