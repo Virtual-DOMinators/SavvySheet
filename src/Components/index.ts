37d4cc9ed2470235660d8486090822b6474e4023
Tom Larsson <tmlsn@hotmail.com>
2025-09-26 14:54:43 +0200

    Delade upp till olika filer för bättre struktur och användning

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,3 @@
 export { default as EditableTable } from './EditableTable';
-export { default as ExportButton } from './ExportButton';
 export type { ExportColumn } from './ExportButton';
 export { default as Header } from './Header';
