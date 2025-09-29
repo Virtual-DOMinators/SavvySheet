a5e9e3ce705c18c3f0177b3568fa2ab1bde5848e
Tom Larsson <tmlsn@hotmail.com>
2025-09-29 15:04:28 +0200

    fix för teman och ändring av färger i css.

---
+++ src/styles/daisyThemes.ts
@@ -0,0 +1,14 @@
+export const daisyThemes = [
+  'light',
+  'dark',
+  'cupcake',
+  'bumblebee',
+  'emerald',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'valentine',
+];
+
+export default daisyThemes;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,4 @@
+export * from './daisyThemes';
+
 import './index.css';
 import './App.css';
