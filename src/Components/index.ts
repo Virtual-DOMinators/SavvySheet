c15eae1bc421732ed6de7c7c4db2a27624f2522c
Tom Larsson <tmlsn@hotmail.com>
2025-09-29 15:02:20 +0200

    uppdaterar för export

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as EditableTable } from './EditableTable';
 export type { ExportColumn } from './ExportButton';
+export { default as UploadFile } from './UploadFile';
 export { default as Header } from './Header';
