749e730088f3002f9a92f8b458836cc36e9e53e1
Tom Larsson <tmlsn@hotmail.com>
2025-10-01 15:40:04 +0200

    tog bort onödig styling

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <>
       <Header />
-      <main className="w-full max-w-3xl mx-auto px-2 md:px-8 mb-2">
+      <main className="w-full max-w-3xl mx-auto">
         <Suspense fallback={<Spinner />}>
           <UploadFile onDataParsed={handleDataParsed} />
         </Suspense>
